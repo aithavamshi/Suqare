422da317abe5239b1ac9254f63c3f6db505ab040
Aitha vamshi <aithavamshi31@gmail.com>
2026-09-15 18:08:48 +0530

    Removed the testing comment

--- square/nb_bronze_square.Notebook/notebook-content.py
+++ square/nb_bronze_square.Notebook/notebook-content.py
@@ -30,7 +30,7 @@ import requests
 import json
 from datetime import datetime, timezone
 from pathlib import Path  
-#testing  ffggg
+
 
 # ============================================================
 # SQUARE CONFIGURATION
